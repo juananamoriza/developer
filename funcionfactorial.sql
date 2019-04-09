c246815c8821a0dc8d98089970cb8237b5bae2f2
dw1a_alum21 <dw1a_alum21@DW1A-02.inform.local>
2019-04-09 10:07:02 +0200

    cambios de Tantan

--- funcionfactorial.sql
+++ funcionfactorial.sql
@@ -1,11 +1,10 @@
 create or replace function FACTORIAL (num2 number) return number is
 	fact number(30):=1;
 begin
-	for i in 2..num2 loop
+	for i in 2..num2 loop --Cambios de Tantan
     	fact:=fact*i; --corregido por L -- pisado lo de tania
 	return fact;
 end;
 /
-
 -- Cambios desde rubik3
---Cambios de Tantan
+
