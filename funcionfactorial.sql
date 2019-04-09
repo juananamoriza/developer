fc3ffeff84608d69152325c8616ef92e23384791
tantanfpm <>
2019-04-09 09:12:19 +0200

    Fallo provocado -Tania-

--- funcionfactorial.sql
+++ funcionfactorial.sql
@@ -2,9 +2,9 @@ create or replace function FACTORIAL (n number) return number is
 	fact number(30):=1;
 begin
 	for i in 2..n loop
-		fact:=i*i;
+		fact:=fact*i;
 	end loop;
-	return fact;
+	return facto;
 end;
 /
 
