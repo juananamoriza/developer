be3d5740a6548fb0f12791dd892da40a38497ccd
ibai <>
2019-04-09 09:36:44 +0200

    hidjsfkjsdlm,

--- funcionfactorial.sql
+++ funcionfactorial.sql
@@ -2,7 +2,7 @@ create or replace function FACTORIAL (n number) return number is
 	fact number(30):=1;
 begin
 	for i in 2..n loop
-		fact:=fact*i;
+		fact:=fact*i;-- ibai es jesucristo y lo ha dejado mejor
 	end loop;
 	return fact;
 end;
