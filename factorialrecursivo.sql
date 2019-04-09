098dc7d6c75cab0decb54770e976189fbc02a9b7
julen <>
2019-04-09 11:15:54 +0200

    co

--- factorialrecursivo.sql
+++ factorialrecursivo.sql
@@ -1,9 +1,9 @@
-create or replace function FACTORIAL (num number) return number is
+create or replace function FACTORIAL (num20 number) return number is
 begin
-	if num=1 then
+	if num20=1 then
 		return 1;
 	else
-		return num*factorial(num-1);
+		return num20*factorial(num20-1);
 	end if;
 end;
 /
