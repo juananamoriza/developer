05c873472a435e4963b722d22f2a148824068243
ibai <>
2019-04-09 10:01:10 +0200

    hrthgf

--- factorialrecursivo.sql
+++ factorialrecursivo.sql
@@ -1,9 +1,9 @@
 create or replace function FACTORIAL (num6 number) return number is
 begin
-	if num14=1 then
-		return 1;
-	else
+    if num6=1 then
+        return 1
+    else
 		return num6*factorial(num6-1);
-	end if;
+    end if;
 end;
 /
